4dba729fabf1474d2664a6de72960b6d1998ccdc
Aideed <mwangaaideed@gmail.com>
2024-01-02 09:10:37 +0300

    map configurations added to the android manifest file

--- src/screens/ShopsMapScreen.js
+++ src/screens/ShopsMapScreen.js
@@ -1,16 +1,34 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function ShopsMapScreen(){
     return(
-        <View style={{flex:1, alignItems:'center',justifyContent:'center'}}>
-            <Text>Shops Screen</Text>
-        </View>
-    )
+        <View style={styles.container}>
+            <MapView
+                provider={PROVIDER_GOOGLE}
+                 //remove if not using Google Maps//
+                style={styles.map}
+                region={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.015,
+                    longitudeDelta: 0.0121,
+                }}
+            >
+        </MapView>
+    </View>
+)
 }
 
-const styles = StyleSheet.create(
-    {
-
-    }
-)
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        flex:1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        },
+        map: {
+            ...StyleSheet.absoluteFillObject,
+        },
+})
